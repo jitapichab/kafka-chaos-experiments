6216bdc4fdddaa8e1695c562b31d1cf1226382d6
Jorge Tapicha <jitapichab@gmail.com>
2024-07-20 17:52:13 -0500

    github actions file
    Signed-off-by: Jorge Tapicha <jitapichab@gmail.com>

--- code/poke-order-api/start.sh
+++ code/poke-order-api/start.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Run Alembic migrations
-alembic upgrade head
+RETRIES=5
+
+WAIT=5
+
+for i in $(seq 1 $RETRIES); do
+        echo "Attempt $i: Running alembic upgrade head..."
+        alembic upgrade head && break
+
+        if [ "$i" -eq $RETRIES ]; then
+            echo "Max retries reached. Alembic upgrade failed."
+            exit 1
+        fi
+
+        echo "Alembic upgrade failed. Retrying in $WAIT seconds..."
+        sleep $WAIT
+    done
 
 # Start the FastAPI application
 uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
